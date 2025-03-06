d785ce86ec8e258739f5d07e22a623d4f5e9fe52
Jenloke <mjenloke@gmail.com>
2025-03-06 23:30:23 +0800

    remove uneeded code

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-# Basic File Reading Python
-# file = [100,200,500,1000, 2000,5000,10000]
-# log = 0
-# for i in range(3):
-#   for x in file:
-#     file1 = open(f"./Dataset/{i+1}/{x}/knapPI_{i+1}_{x}_1000_1", "r")
-#     log = log + 1
-#     print(file1.read)
-# print(log)
-
-
-# with open(f"./Dataset/{1}/{100}/knapPI_{1}_{100}_1000_1", "r") as file:
-#   content = file.read()
-#   print(content)
-
-# knapsackProblem = "knapPI_1_100_1000_1"
-# optimalSolution = "knapPI_1_100_1000_1a"
-# optimalKnapsackValue = "knapPI_1_100_1000_1o"
-
-# print(open(f"./Dataset/{1}/{100}/knapPI_{1}_{100}_1000_1", "r").read())
-
 import numpy as np
+
+# size = [100,200,500,1000, 2000,5000,10000]
+# file = [1, 2, 3]
+
+file = 1
+size = 100
 
 # File 1
 # from binary file to a np array where each line has become a list
-knapsackProblem = open(f"./Dataset/{1}/{100}/knapPI_{1}_{100}_1000_1", "r").readlines()
+knapsackProblem = open(f"./Dataset/{file}/{size}/knapPI_{file}_{size}_1000_1", "r").readlines()
 new_knapsackProblem = np.array([x.split() for x in knapsackProblem], dtype=int).astype(int)
 
 # get first element cause it contains the total items and the knapsack's capacity
@@ -36,20 +21,21 @@ np_knapsackProblem = new_knapsackProblem[1:]
 
 # File 2
 # Optimal Solution Array of 1 and 0 to NumPy Array of Boolean Values
-optimalSolution = open(f"./Dataset/{1}/{100}/knapPI_{1}_{100}_1000_1a", "r").read()
-np_booleanArray = np.array(optimalSolution.split(), dtype=int).astype(bool)
+optimalSolution = open(f"./Dataset/{file}/{size}/knapPI_{file}_{size}_1000_1a", "r").read()
+# np_booleanArray = np.array(optimalSolution.split(), dtype=int).astype(bool)
+np_booleanArray = np.array(optimalSolution.split(), dtype=int)
 # print(len(np_booleanArray))
 
 # File 3
-optimalKnapsackValue: int = int(open(f"./Dataset/{1}/{100}/knapPI_{1}_{100}_1000_1o", "r").read())
-# print(optimalKnapsackValue)
+optimalKnapsackValue: int = int(open(f"./Dataset/{file}/{size}/knapPI_{file}_{size}_1000_1o", "r").read())
+print(optimalKnapsackValue)
 
 # Combining File 1 && 3
 value, weight = np_knapsackProblem.T
-# value, weight = np.hsplit(np_knapsackProblem, 2)
-# value = value.flatten()
-# weight = weight.flatten()
 combination = np.stack((value, weight, np_booleanArray), axis=1)
+
+# print(np_booleanArray)
+print(np.dot(np_booleanArray, value))
 
 # Correct
 # print(len(np_booleanArray))
@@ -58,14 +44,6 @@ combination = np.stack((value, weight, np_booleanArray), axis=1)
 # print(np_booleanArray)
 # print(weight)
 # print(value)
-
-# print(np_booleanArray[6])
-# print(np_knapsackProblem[6])
-# print(combination[6])
-
-# print(np_booleanArray[10])
-# print(np_knapsackProblem[10])
-# print(combination[10])
 
 # print(np_booleanArray[37])
 # print(np_knapsackProblem[37])
