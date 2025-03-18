1a322383dbb7be06add371c7ad4e336e9416b7d3
Jenloke <mjenloke@gmail.com>
2025-03-18 11:14:20 +0800

    add time tracking

--- pso.py
+++ pso.py
@@ -1,8 +1,12 @@
-import numpy as np
 import json
+import numpy as np
+from datetime import datetime
 
-file = 1
-size = 500
+# Randomly generate item values and weights
+# np.random.seed(42)
+
+file = 3
+size = 200
 
 knapsackProblem = open(f"./Dataset/{file}/{size}/knapPI_{file}_{size}_1000_1", "r").readlines()
 new_knapsackProblem = np.array([x.split() for x in knapsackProblem], dtype=int).astype(int)
@@ -11,27 +15,22 @@ knapsackTotalItems, knapsackCapacity = new_knapsackProblem[:1][0]
 print(knapsackTotalItems, knapsackCapacity)
 # get the rest cause it is the list of items for the 0/1 knapsack problem
 np_knapsackProblem = new_knapsackProblem[1:]
-
 # File 2
 # Optimal Solution Array of 1 and 0 to NumPy Array of Boolean Values
 optimalSolution = open(f"./Dataset/{file}/{size}/knapPI_{file}_{size}_1000_1a", "r").read()
 np_booleanArray = np.array(optimalSolution.split(), dtype=int)
 # print(np_booleanArray)
-
 # File 3
 optimalKnapsackValue: int = int(open(f"./Dataset/{file}/{size}/knapPI_{file}_{size}_1000_1o", "r").read())
 print(optimalKnapsackValue)
-
 # Combining File 1 && 3
 value, weight = np_knapsackProblem.T
+
+start_time = datetime.now()
 
 # Problem Parameters
 n_items = len(np_knapsackProblem)  # Number of items
 max_weight = knapsackCapacity  # Knapsack capacity
-
-# Randomly generate item values and weights
-np.random.seed(42)
-
 values = value
 weights = weight
 
@@ -85,6 +84,9 @@ for _ in range(max_iterations):
   if np.max(pBest_scores) > gBest_score:
     gBest = pBest[np.argmax(pBest_scores)].copy()
     gBest_score = np.max(pBest_scores)
+
+end_time = datetime.now()
+print(end_time - start_time)
 
 # Output best solution
 print("Best value obtained:", gBest_score)
